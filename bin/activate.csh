3e8e3aa9a12fe5b83e8300603dee1b89872c3251
Ank-Done <134251785+Ank-Done@users.noreply.github.com>
2025-07-08 14:24:02 -0600

    Add files via upload

--- bin/activate.csh
+++ bin/activate.csh
@@ -9,17 +9,17 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/ank/Documents/REporte/venv
+setenv VIRTUAL_ENV /home/ank/Documents/REporte/data_env
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
-setenv VIRTUAL_ENV_PROMPT venv
+setenv VIRTUAL_ENV_PROMPT data_env
 
 
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "("venv") $prompt:q"
+    set prompt = "("data_env") $prompt:q"
 endif
 
 alias pydoc python -m pydoc
--- bin/activate.fish
+++ bin/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ank/Documents/REporte/venv
+set -gx VIRTUAL_ENV /home/ank/Documents/REporte/data_env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
-set -gx VIRTUAL_ENV_PROMPT venv
+set -gx VIRTUAL_ENV_PROMPT data_env
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) venv (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) data_env (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
