71f1dbccc62713f6123b69c45b4da192cafb13aa
Ank <134251785+Ank-Done@users.noreply.github.com>
2025-08-01 20:36:34 +0000

    Add files via upload

--- CyberiaHumanCopy/app.py
+++ CyberiaHumanCopy/app.py
@@ -2043,4 +2043,9 @@ def reporte_coberturas():
                                current_lang=get_language())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # SSL context for HTTPS
+    import ssl
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('cert.pem', 'key.pem')
+    
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=context)
